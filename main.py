9588b5a9cfefbf43fa1e46cef332337939547fd7
Leon Röderer <leon.roederer@t-online.de>
2025-10-26 19:36:49 +0100

    added icon

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from db import init_db
 from ui.main_window import MainWindow
 
 def main():
     init_db()
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("assets/icons/icon_512x512.ico"))
     win = MainWindow()
     win.show()
     sys.exit(app.exec())
