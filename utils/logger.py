8fa54013c6a5e227d74b02e5ef36a25458be0c4b
Leon Röderer <leon.roederer@t-online.de>
2025-11-08 14:11:27 +0100

    fixed logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,19 +1,14 @@
 # utils/logger.py
-import pathlib
+from pathlib import Path
 import datetime
 import os
 
 class Logger:
     def __init__(self, log_dir=None, log_file_name="default.log"):
-        """
-        log_dir: Pfad zum Ordner, in dem die Log-Datei liegen soll.
-                 Wenn None, dann im gleichen Ordner wie die exe.
-        """
         if log_dir is None:
-            self.log_dir = pathlib.Path(__file__).parent.parent  # Root-Ordner des Projekts
-        else:
-            self.log_dir = pathlib.Path(log_dir)
-
+            # Standard: AppData\Local\SVM-Jugend\logs
+            log_dir = Path(os.environ["LOCALAPPDATA"]) / "SVM-Jugend" / "logs"
+        self.log_dir = Path(log_dir)
         self.log_dir.mkdir(parents=True, exist_ok=True)
         self.log_file = self.log_dir / log_file_name
 
